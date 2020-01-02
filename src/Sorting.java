eeb56015fc01a2673b19b5a97711e9b19479c730
Debug-2020 <debug2020@protonmail.com>
2020-01-02 15:52:04 +0800

    4118-56188-Sorting-result1575172373426

--- src/Sorting.java
+++ src/Sorting.java
@@ -29,8 +29,10 @@ public final class Sorting {
 
 	public boolean isSorted(int[] a) {
 		for (int i = 0; i < a.length; i++) {
-			if (a[i] > a[i + 1]) {
-				return false;
+			for (int j = i+1; j < a.length; j++) {
+				if (a[i] > a[j]) {
+					return false;
+				}
 			}
 		}
 		return true;
